b715cde073832edaff33e1dbaae00d8c922de8f9
Ashiqur Rahman <arshohag8@gmail.com>
2022-06-16 01:00:11 +0600

    Create 2022_06_15_185217_create_urls_table.php

---
+++ database/migrations/2022_06_15_185217_create_urls_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('urls', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('hash', 6);
+            $table->text('old_url');
+            $table->text('new_url');                        
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('urls');
+    }
+};
