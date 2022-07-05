b240375ef40314f0cbacfd1c5e584c160301011c
Sohag Hasan <shohaghasan575@gmail.com>
2022-07-05 10:15:52 +0600

    update | logic to create unique hash

--- app/Services/UrlHash.php
+++ app/Services/UrlHash.php
@@ -4,16 +4,56 @@ namespace App\Services;
 
 class UrlHash
 {
+
     /**
-     * Create a short, fairly unique, urlsafe hash for the Input URL.
+     * Get all existing hash
+     */
+    private $existingHash;
+
+    public function __construct()
+    {
+        $this->existingHash = Url::all('hash')->pluck('hash')->toArray();
+    }
+
+    /**
+     * Create a short, unique, urlsafe hash for the Input URL.
      */
 
     public function createUrlHash($url, $length = 6)
+    {
+        $hash = $this->_makeHash($url, $length);
+        return $this->_generateUniqueHash($hash, $url, $length);
+    }
+
+    /**
+     * Check for unique hash
+     */
+    private function _isUniqueHash($newHash)
+    {
+        return !in_array($newHash, $this->existingHash);
+    }
+
+    /**
+     * Make the hash string
+     */
+    private function _makeHash($url, $length)
     {
         $hash_base64 = base64_encode(hash('sha256', $url, true));
         $hash = strtr($hash_base64, '+/', '-_');
         $hash = rtrim($hash, '=');
 
         return substr($hash, 0, $length);
+    }
+
+    /**
+     * Generate unique hash
+     */
+    private function _generateUniqueHash($hash, $url, $length)
+    {
+        if($this->_isUniqueHash($hash)){
+            return $hash;
+        }
+
+        return $this->_makeHash($url, $length)
     }
 }
